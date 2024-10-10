9b12f358eb0b596dc5b8662494fa7f1d62244d4a
Muhammad Ali Raza <155912766+Raza0043@users.noreply.github.com>
2024-10-11 01:10:23 +0500

    Add files via upload

---
+++ Assign # 12/app.js
@@ -0,0 +1,39 @@
+// Question no  # 01
+
+// 1. Create a simple Hat-Tail program using with Math.random() method.
+
+// function flipCoin() {
+//     var randomNumber = Math.random();  // Generates a random number between 0 and 1
+
+//     if (randomNumber < 0.5) {
+//         document.getElementById("result").innerHTML = "It's Heads!";
+//     } else {
+//         document.getElementById("result").innerHTML = "It's Tails!";
+//     }
+// }
+
+// Question no  # 02
+// 2. Create a simple Dice roll program using with Math.random() method.
+
+// var diceRoll = Math.floor(Math.random() * 6) + 1;
+// document.write("You rolled: " + diceRoll);
+
+
+// Question no  # 03
+// 3. Create a simple random password generator for length of password is 10.
+
+
+// var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+// var password = ""; 
+
+
+// for (var i = 0; i < 10; i++) {
+  
+//     var randomIndex = Math.floor(Math.random() * characters.length);
+   
+//     password += characters[randomIndex];
+// }
+
+
+// document.write("Generated Password: " + password);
+
